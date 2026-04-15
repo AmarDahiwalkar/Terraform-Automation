138b256987991852f0733f5bb0977ad45b0a3570
AmarDahiwalkar <amardahiwalkar@gmail.com>
2026-04-15 17:14:00 +0530

    1st

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "young-minds-app-project-terraform-state-123-4507"
+    key = "main"
+    region = "ap-south-1"
+    dynamodb_table = "my-dynamodb-table"
+  }
+}
